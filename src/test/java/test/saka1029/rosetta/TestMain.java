246a4d37730f23216a90c556d75bff8badced4df
saka1029 <saka1029@gmail.com>
2026-07-28 23:29:44 +0900

    ..

--- src/test/java/test/saka1029/rosetta/TestMain.java
+++ src/test/java/test/saka1029/rosetta/TestMain.java
@@ -9,21 +9,26 @@ import saka1029.rosetta.Main;
 public class TestMain {
 
     @Test
-    public void testEval() {
+    public void testEvalInteger() {
         assertEquals(Integer.valueOf(6), Main.eval("(+ 1 2 3)"));
     }
 
+    // @Test
+    // public void testEvalSymbol() {
+    //     assertEquals(Integer.valueOf(6), Main.eval("(def s 'z) s"));
+    // }
+
     @Test
     public void testEvalTestLisp() {
-        assertEquals(Integer.valueOf(288),
+        assertEquals(Integer.valueOf(144),
             Main.eval("""
                 (def sum (lambda (a b) (+ a b)))
 
                 (def ft 14)
 
                 (def fib (lambda (n)
-                (if (<= n 2)
-                    2
+                (if (<= n 1)
+                    n
                     (+ (fib (- n 1)) (fib (- n 2))))))
 
                 (sum 12 (- ft 2))
